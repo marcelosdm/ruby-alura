33ae6da074e3cb164e0eab0f475eb9429ce4a36f
Marcelo Melo STA <marcelosdm@gmail.com>
2016-10-05 11:01:04 -0300

    Arquivos aula 03

--- maior_ou_menor.rb
+++ maior_ou_menor.rb
@@ -1,32 +1,53 @@
-puts "Bem-vindo ao jogo da adivinhação"
-puts "Qual é o seu nome?"
-nome = gets
+def da_boas_vindas
+	puts "Bem-vindo ao jogo da adivinhação"
+	puts "Qual é o seu nome?"
+	nome = gets
 
-puts "\n\n\n\n\n\n"
-puts "Começaremos o jogo para você, " + nome
+	puts "\n\n\n\n\n\n"
+	puts "Começaremos o jogo para você, " + nome
+end
 
-puts "Escolhendo um número secreto entre 0 e 200..."
-numero_secreto = 175
-puts "Escolhido... que tal adivinhar nosso número secreto de hoje?"
+def sorteia_numero_secreto
+	puts "Escolhendo um número secreto entre 0 e 200..."
+	sorteado = 175
+	puts "Escolhido... que tal adivinhar nosso número secreto de hoje?"
+	sorteado
+end
 
-limite_tentativas = 3
-for tentativa in 1..limite_tentativas
+def pede_um_numero tentativa, limite_tentativas
 	puts "\n\n\n\n"
 	puts "Tentativa " + tentativa.to_s + " de " + limite_tentativas.to_s + " Entre com o seu número:"
 	chute = gets
 	puts "Será que você acertou? Você chutou " + chute
-
-	acertou = numero_secreto == chute.to_i
-
-	if acertou
-		puts "Acertou!"
-		break
-	else
-		if numero_secreto > chute.to_i
-			puts "Errou! O número secreto é maior que seu chute!"
-		else
-			puts "Errou! O número secreto é menor que seu chute!"
-		end
-	end
+	chute.to_i
 end
 
+def verifica_se_acertou numero_secreto, chute
+	acertou = numero_secreto == chute
+	if acertou
+		puts "Acertou!"
+		return true
+	end
+	maior = numero_secreto > chute
+	if maior
+		puts "Errou! O número secreto é maior!"
+			
+		else
+			puts "Errou! O número secreto é menor!"
+			
+		end
+end
+
+da_boas_vindas
+numero_secreto = sorteia_numero_secreto
+
+
+limite_tentativas = 3
+for tentativa in 1..limite_tentativas
+	chute = pede_um_numero tentativa,limite_tentativas
+
+	if verifica_se_acertou numero_secreto, chute
+		break
+	end
+
+end
--- teste.rb
+++ teste.rb
@@ -1,4 +1,4 @@
 puts "Irei começar a contar"
 for contar in 1..3
-puts "Contando" + contar.to_s
+	puts "Contando" + contar.to_s
 end
